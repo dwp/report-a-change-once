ada345b25fa7e1935e60b1731814e37938893816
Jadene Aderonmu <Jadene.Aderonmu@engineering.digital.dwp.gov.uk>
2026-02-05 16:44:12 +0000

    Personal details page added and my change is not listed here
    GitOrigin-RevId: 64a3c63a6419768ade14439be3052406e3b4b755

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,17 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
+
 // Add your routes here
+
+router.post('/v03-journey-1/starting-question', function (req, res) {
+    const change = req.body['what-do-you-need-to-change']
+    if (change === 'address') {
+        res.redirect('/v03-journey-1/task-list.html')
+    }else if (change === 'other') {
+        res.redirect('/v03-journey-1/my-change-is-not-listed')
+    }else {
+        res.redirect('/v03-journey-1/starting-question')
+    }
+})
