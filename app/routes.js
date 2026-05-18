e45cc58ced299687e98a5146149558bb8bc9ebe2
JonathanThomas <jonathan.thomas1@engineering.digital.dwp.gov.uk>
2026-05-18 17:00:21 +0100

    added v4
    GitOrigin-RevId: 31e3eb63e8d47bf1cda4c5731efc733e1e645e07

--- app/routes.js
+++ app/routes.js
@@ -20,3 +20,34 @@ router.post('/v03-journey-1/starting-question', function (req, res) {
         res.redirect('/v03-journey-1/starting-question')
     }
 })
+
+// Declare the entry point service - customer account
+router.get('/v04/index.html', function(request, response) {
+    response.redirect("/v04/customer-account/account-home")
+})
+
+// Declare the RACO start point - when did you move?
+router.get('/v04/report-a-change-once/index.html', function(request, response) {
+    response.redirect("/v04/report-a-change-once/when-did-you-move")
+})
+
+router.post('/v04/report-a-change-once/when-did-you-move', function (req, res) {
+  const day = req.body["move-date-day"]
+  const month = req.body["move-date-month"]
+  const year = req.body["move-date-year"]
+
+  // Create Date object (month is 0-based in JS)
+  const enteredDate = new Date(year, month - 1, day)
+
+  // Today's date (with time removed)
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  // Check if date is in the future
+  if (enteredDate > today) {
+    return res.redirect('/v04/report-a-change-once/you-cannot-use-this-service/future-date')
+  }
+
+  // Otherwise (today or past)
+  res.redirect('/v04/report-a-change-once/permanent-or-temporary-move')
+})
