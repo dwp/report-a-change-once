e120454c6588217b7be0481a25da8dbea22a4a93
JonathanThomas <jonathan.thomas1@engineering.digital.dwp.gov.uk>
2026-05-19 17:22:32 +0100

    added v4 files
    GitOrigin-RevId: 05bb31b921db25a8d5ab4e81435d5ac460b7304e

---
+++ app/routes/v04.js
@@ -0,0 +1,83 @@
+module.exports = function (router) {
+
+// Declare the entry point service - customer account
+router.get('/v04/index.html', function(request, response) {
+    response.redirect("/v04/customer-account/account-home")
+})
+
+// Declare the RACO start point - when did you move?
+router.get('/v04/report-a-change-once/index.html', function(request, response) {
+    response.redirect("/v04/report-a-change-once/when-did-you-move")
+})
+
+router.post('/v04/report-a-change-once/when-did-you-move', function (req, res) {
+  const day = req.body["move-date-day"]
+  const month = req.body["move-date-month"]
+  const year = req.body["move-date-year"]
+
+  // Create Date object (month is 0-based in JS)
+  const enteredDate = new Date(year, month - 1, day)
+
+  // Today's date (with time removed)
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  // Check if date is in the future
+  if (enteredDate > today) {
+    return res.redirect('/v04/report-a-change-once/you-cannot-use-this-service/future-date')
+  }
+
+  // Otherwise (today or past)
+  res.redirect('/v04/report-a-change-once/permanent-or-temporary-move')
+})
+
+router.post('/v04/report-a-change-once/permanent-or-temporary-move', function (req, res) {
+
+  // Make a variable and give it the value from 'permanentTempMove' to take the value of the radio list name
+  var permanentTempMove = req.session.data['permanentOrTemporary']
+
+  // Check whether the variable matches a condition
+  if (permanentTempMove == "permanent"){
+    // Send user to next page
+    res.redirect('/v04/report-a-change-once/which-country-do-you-live-in')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/v04/report-a-change-once/you-cannot-use-this-service/temporary-address')
+  }
+
+})
+
+router.post('/v04/report-a-change-once/which-country-do-you-live-in', function (req, res) {
+
+  // Make a variable and give it the value from 'permanentTempMove' to take the value of the radio list name
+  var country = req.session.data['country']
+
+  // Check whether the variable matches a condition
+  if (country == "englandAndWales"){
+    // Send user to next page
+    res.redirect('/v04/report-a-change-once/address/what-is-your-new-address')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/v04/report-a-change-once/you-cannot-use-this-service/not-in-england-and-wales')
+  }
+
+})
+
+router.post('/v04/report-a-change-once/address/what-is-your-new-address', function(request, response) {
+    response.redirect("/v04/report-a-change-once/address/select-your-new-address")
+})
+
+// Drop out screens
+router.post('/v04/report-a-change-once/you-cannot-use-this-service/future-date', function(request, response) {
+    response.redirect("/v04/customer-account/account-home")
+})
+
+router.post('/v04/report-a-change-once/you-cannot-use-this-service/temporary-address', function(request, response) {
+    response.redirect("/v04/customer-account/account-home")
+})
+
+router.post('/v04/report-a-change-once/you-cannot-use-this-service/not-in-england-and-wales', function(request, response) {
+    response.redirect("/v04/customer-account/account-home")
+})
+
+}
